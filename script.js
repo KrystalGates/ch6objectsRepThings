80450b9996500909fd0dd3b81694034850320f4b
krystal gates <krystal_sabrina@yahoo.com>
2019-06-06 13:01:11 -0500

    initial html and JS instructions for exercise

---
+++ script.js
@@ -0,0 +1,4 @@
+// Your job is to define the different objects and arrays, 
+// their structure, and the corresponding properties for each, 
+// to represent the following information about Elizabeth's campaign.
+
